65e798356b54692c6b7840f2d5feb121a9489e9e
hhaa1382 <mehranfarhamid82@gmail.com>
2024-06-25 01:24:44 +0330

    x add

--- find_secret.py
+++ find_secret.py
@@ -4,12 +4,11 @@ class FindSecret:
         self.n = n
         self.p = p
         self.Y = Y
+        self.x = [i for i in range(1, n)]
 
     def inverse(self, number):
         number = number % self.p
-        if number == 0:
-            return 0
-        for i in self.p:
-            if (i*number)%self.p == 1:
+        for i in range(1, self.p):
+            if (i * number) % self.p == 1:
                 return i
         return -1

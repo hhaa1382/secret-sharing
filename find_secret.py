905b47ed6baa35d2149306ce29bf41f14c422c3f
hhaa1382 <mehranfarhamid82@gmail.com>
2024-06-25 01:29:14 +0330

    calculation multiplication add

--- find_secret.py
+++ find_secret.py
@@ -4,7 +4,7 @@ class FindSecret:
         self.n = n
         self.p = p
         self.Y = Y
-        self.x = [i for i in range(1, n)]
+        self.x = [i for i in range(1, t)]
 
     def inverse(self, number):
         number = number % self.p
@@ -12,3 +12,13 @@ class FindSecret:
             if (i * number) % self.p == 1:
                 return i
         return -1
+
+    def calculateMultiplication(self, i):
+        temp = 1
+        for j in range(self.p):
+            if i != j:
+                inv = self.inverse(self.x[j]-self.x[i])
+                if inv == -1:
+                    raise Exception(f"{self.p} is not invertible for sum numbers !!")
+                temp *= inv * self.x[j]
+        return temp

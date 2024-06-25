1b08577542d0a1a45c7656ff17c457fc02778cd3
hhaa1382 <mehranfarhamid82@gmail.com>
2024-06-26 00:50:26 +0330

    function generator add

--- make_function.py
+++ make_function.py
@@ -1,6 +1,21 @@
+from numpy import random
+
+
 class MakeFunction:
     def __init__(self, S, t, n, p):
         self.p = p
         self.S = S
         self.t = t
         self.n = n
+
+        self.func = [self.S]
+
+    def randomGenerator(self):
+        return random.rand(self.t)
+
+    def generateFunction(self):
+        num = self.randomGenerator()
+        for i in range(1, self.t+1):
+            self.func.append((num, i))
+
+
